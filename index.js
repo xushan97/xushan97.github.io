45569d37ebf7cd86a60faeb32e09b915db181178
xushan97 <50835687+xushan97@users.noreply.github.com>
2019-05-21 01:03:26 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,188 +1,225 @@
-function App() {
-  const conf = {
-    el: 'canvas',
-    fov: 75,
-    cameraZ: 140,
-    background: 0x00001a,
-    numCircles: 40,
-    numPointsPerCircle: 1000 };
+/*
+  Still to do:
+  - Make the animation time based so it can run faster and not frame rate dependant
+  - Make the thicknes bigger on random areas of the leminescate
+*/
 
-
-  let renderer, scene, camera, cameraCtrl, startTime;
-  let width, height;
-
-  const numPoints = conf.numCircles * conf.numPointsPerCircle;
-  let points;
-
-  const mouse = new THREE.Vector2(0.1, 0.5);
-  const { randFloat: rnd, randFloatSpread: rndFS } = THREE.Math;
-
-  init();
-
-  function init() {
-    renderer = new THREE.WebGLRenderer({ canvas: document.getElementById(conf.el) });
-    camera = new THREE.PerspectiveCamera(conf.fov);
-    camera.far = 10000;
-    camera.position.z = conf.cameraZ;
-    cameraCtrl = new THREE.OrbitControls(camera, renderer.domElement);
-    cameraCtrl.enableKeys = false;
-    cameraCtrl.enableDamping = true;
-    cameraCtrl.dampingFactor = 0.1;
-    cameraCtrl.rotateSpeed = 0.1;
-
-    updateSize();
-    window.addEventListener('resize', updateSize, false);
-    renderer.domElement.addEventListener('mousemove', e => {
-      mouse.x = e.clientX / width * 2 - 1;
-      mouse.y = -(e.clientY / height) * 2 + 1;
-    });
-
-    startTime = Date.now();
-    initScene();
-    animate();
-  }
-
-  function initScene() {
-    scene = new THREE.Scene();
-    if (conf.background) scene.background = new THREE.Color(conf.background);
-
-    const positions = new Float32Array(numPoints * 3);
-    const colors = new Float32Array(numPoints * 3);
-    const sizes = new Float32Array(numPoints);
-    const rotations = new Float32Array(numPoints);
-    const sCoef = new Float32Array(numPoints);
-    const ci = new Float32Array(numPoints);
-    const cj = new Float32Array(numPoints);
-
-    const position = new THREE.Vector3();
-    const cscale = chroma.scale([0x00b9e0, 0xff880a, 0x5f1b90, 0x7ec08d]);
-    let index, color;
-    for (let i = 0; i < conf.numCircles; i++) {
-      for (let j = 0; j < conf.numPointsPerCircle; j++) {
-        index = conf.numPointsPerCircle * i + j;
-        position.set(rndFS(10), rndFS(10), rndFS(1.5 * (conf.numCircles - i)));
-        position.toArray(positions, index * 3);
-        color = new THREE.Color(cscale(rnd(0, 1)).hex());
-        color.toArray(colors, index * 3);
-        sizes[index] = rnd(2, 30);
-        sCoef[index] = rnd(0.0001, 0.005);
-        rotations[index] = rnd(0, Math.PI);
-        ci[index] = i + 1;
-        cj[index] = j + 1;
-      }
-    }
-
-    const geometry = new THREE.BufferGeometry();
-    geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
-    geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3));
-    geometry.addAttribute('size', new THREE.BufferAttribute(sizes, 1));
-    geometry.addAttribute('rotation', new THREE.BufferAttribute(rotations, 1));
-    geometry.addAttribute('sCoef', new THREE.BufferAttribute(sCoef, 1));
-    geometry.addAttribute('ci', new THREE.BufferAttribute(ci, 1));
-    geometry.addAttribute('cj', new THREE.BufferAttribute(cj, 1));
-
-    const material = new THREE.ShaderMaterial({
-      uniforms: {
-        uTime: { value: 0 },
-        uMouse: { value: mouse },
-        uTexture: { value: new THREE.TextureLoader().load('https://klevron.github.io/codepen/misc/star.png') },
-        uRCoef: { value: 7 },
-        uACoef: { value: 2 * Math.PI / conf.numPointsPerCircle } },
-
-      vertexShader: `
-      uniform float uTime;
-      uniform vec2 uMouse;
-      uniform float uRCoef;
-      uniform float uACoef;
-      attribute vec3 color;
-      attribute float size;
-      attribute float rotation;
-      attribute float sCoef;
-      attribute float ci;
-      attribute float cj;
-      varying vec4 vColor;
-      varying float vRotation;
-      void main() {
-        vColor = vec4(color, 1.);
-        vRotation = rotation;
-
-        float rx = ci * uRCoef;
-        float ry = rx * 0.6;
-        float a = cj * uACoef;
-        float t = uTime * 0.00005;
-        vec2 p2 = vec2(cos(a+t) * rx, sin(a+t) * ry) + position.xy;
-
-        a = ci * 0.25 - t;
-        float ca = cos(a), sa = sin(a);
-        p2 = p2 * mat2(ca, -sa, sa, ca);
-
-        vec3 p = vec3(p2, position.z);
-
-        vec4 mvPosition = modelViewMatrix * vec4(p, 1.);
-        gl_Position = projectionMatrix * mvPosition;
-
-        float psize = size * (200. / -mvPosition.z);
-        gl_PointSize = psize * (1. + .5*sin(uTime*sCoef + position.x));
-      }
-    `,
-      fragmentShader: `
-      uniform sampler2D uTexture;
-      varying vec4 vColor;
-      varying float vRotation;
-      void main() {
-        vec2 v = gl_PointCoord - .5;
-        float ca = cos(vRotation), sa = sin(vRotation);
-        mat2 rmat = mat2(ca, -sa, sa, ca);
-        gl_FragColor = vColor * texture2D(uTexture, v*rmat + .5);
-      }
-    `,
-      blending: THREE.AdditiveBlending,
-      depthTest: false,
-      transparent: true });
-
-
-    points = new THREE.Points(geometry, material);
-    points.rotation.x = -0.5;
-    scene.add(points);
-
-    renderer.domElement.addEventListener('mouseup', e => {
-      randomColors();
-    });
-  }
-
-  function randomColors() {
-    startTime = Date.now();
-    const cscale = chroma.scale([chroma.random(), chroma.random(), chroma.random(), chroma.random()]);
-    const colors = points.geometry.attributes.color.array;
-    let j, color;
-    for (let i = 0; i < numPoints; i++) {
-      j = i * 3;
-      color = cscale(rnd(0, 1));
-      colors[j] = color.get('rgb.r') / 0xff;
-      colors[j + 1] = color.get('rgb.g') / 0xff;
-      colors[j + 2] = color.get('rgb.b') / 0xff;
-    }
-    points.geometry.attributes.color.needsUpdate = true;
-  }
-
-  function animate() {
-    requestAnimationFrame(animate);
-
-    const time = Date.now() - startTime;
-    points.material.uniforms.uTime.value = time;
-    points.rotation.z += -mouse.x * 0.01;
-
-    if (cameraCtrl) cameraCtrl.update();
-    renderer.render(scene, camera);
-  }
-
-  function updateSize() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-  }
+function square(x){return x*x;}
+function heart_shape(t, a) {
+  var x = 16 * (Math.sin(t) * Math.sin(t) * Math.sin(t));
+  var y = 13 * Math.cos(t) - 5 * Math.cos(2*t) - 2 * Math.cos(3*t) - Math.cos(4*t);
+  return [x*a/12, y*a/15 * -1];
 }
 
-App();
+function rotate_point(pointX, pointY, originX, originY, angle, extrude) {
+  var slope = Math.atan2(pointY - originY, pointX - originX);
+  pointY = originY + Math.sin(slope) * extrude;
+  pointX = originX + Math.cos(slope) * extrude;
+  
+	angle = angle * Math.PI / 180.0;
+	return {
+		x: Math.cos(angle) * (pointX-originX) - Math.sin(angle) * (pointY-originY) + originX,
+		y: Math.sin(angle) * (pointX-originX) + Math.cos(angle) * (pointY-originY) + originY
+	};
+}
+
+var canvas = document.getElementsByTagName('canvas')[0];
+var ctx = canvas.getContext('2d');
+
+/*
+  Some global variables
+*/
+
+var center = [400,250],
+    pi = Math.PI,
+    lob = heart_shape;
+/* color, maxthicknes, minthickness, maxlength*/
+var colors = [
+  ["rgba(254,68,68,0.4)", 30, 20, pi/4],
+  ["rgba(255,85,85,0.5)", 15, 10, pi/2],
+  ["rgba(151,26,22,0.5)",  10, 8,  pi],
+  ["rgba(250,240,240,0.8)", 10, 8, 1.5*pi],
+  ["rgba(50,11,4,0.7)",     5, 3,  1.5*pi]
+];
+
+/*
+  A loop object
+*/
+function InfinityLoop(){
+  var pickedColor = colors[Math.round(Math.random() * (colors.length -1))];
+  
+  this.a = Math.random() * 50 + 150;
+  //this.length = (Math.random() * 0.75 + 0.25) * pi; //Max = 2*PI
+  this.length = Math.random() * pickedColor[3] + (0.25*pi);
+  this.position = Math.random() * 2 * pi; //0.5 * pi;
+  this.speed = (2 * pi / 100) - (Math.random() * ( pi / 200));
+  this.heightAdjust = Math.random() * 0.2 + 1;
+  this.center = {
+    x: Math.random() * 20 - 10 + center[0],
+    y: Math.random() * 20 - 10 + center[1],
+  }
+
+
+
+  this.color = pickedColor[0];//"rgba(200, 180, 0, 0.75)";
+  this.thickness = Math.random() * pickedColor[1] + pickedColor[2];
+  
+  this.computePath = function(start, length, a) {
+    var main_points = [],
+        extruded_left_points = [],
+        extruded_right_points = [];
+    var segments = Math.round(this.length / this.speed);
+    
+    var lastPoint = null;
+    
+    for(i=0; i<segments; i++) {
+      
+      t = this.speed * i + this.position;
+      if(t > 2 * pi) t = t - 2 * pi;
+      if(t < 0) t = 2 * pi - t;
+      
+      var main_point = lob(t, a);
+          main_point[1] = main_point[1] * this.heightAdjust + this.center.y;
+      var newPoint = {
+        x: main_point[0] + this.center.x,
+        y: main_point[1] 
+      };
+
+      if(lastPoint == null) {
+        var prevPoint = lob(t - this.speed, a);
+        prevPoint[1] = prevPoint[1] * this.heightAdjust + this.center.y;
+        lastPoint = {x: prevPoint[0] + this.center.x, y:prevPoint[1]};
+      }
+
+      var ribbonReductionIndex = ((i / (segments / 200)));
+      //ribbonReductionIndex *= (Math.cos(2*t)+1);
+      var ribbonThickness = Math.sin(pi/2*(ribbonReductionIndex / 100))*this.thickness;      
+
+      var extrudedLeftPoint = rotate_point(
+        lastPoint.x, lastPoint.y, 
+        newPoint.x, newPoint.y, 90, - ribbonThickness/2
+      );
+      extruded_left_points.push(extrudedLeftPoint);
+
+      var extrudedRightPoint = rotate_point(
+        lastPoint.x, lastPoint.y, 
+        newPoint.x, newPoint.y, 90, ribbonThickness/2
+      );
+      extruded_right_points.push(extrudedRightPoint);
+      
+      lastPoint = newPoint;
+    }
+    var points = extruded_left_points.concat(extruded_right_points.reverse());
+    
+    return points;
+  }
+  
+  this.drawPath = function() {
+    var leafPath = this.computePath(
+      this.position, 
+      this.length, 
+      this.a, 5);
+    
+    this.position += this.speed;
+    
+    ctx.fillStyle = this.color;   
+    ctx.beginPath();
+    ctx.moveTo(leafPath[0].x, leafPath[0].y);
+    for(i=1;i<leafPath.length;i++) {
+      ctx.lineTo(leafPath[i].x, leafPath[i].y);
+    }
+    ctx.closePath();
+    ctx.fill();
+    
+    if(this.position > 2 * pi) {
+      this.position = this.position - 2 * pi;
+    }
+  }
+  
+  this.reposition = function(){
+    this.center = {
+      x: Math.random() * 20 - 10 + center[0],
+      y: Math.random() * 20 - 10 + center[1],
+    }
+  }
+  
+  this.tick = function(timestamp){
+    this.drawPath();
+  }
+  
+  return this;
+}
+
+
+
+/*
+  Get the animation started
+*/
+var Scene = function(){
+  
+  var loops = [];
+  var lastTick = 0
+  
+  for(var i=0; i<50; i++) {
+    loops.push(new InfinityLoop());
+  }
+  
+  var clearCanvas = function() {
+    ctx.fillStyle="rgba(200,60,60,.1)";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+  }
+  
+  var animate = function(timestamp) {
+    /*skip frames if more than 60/second*/
+    if(timestamp - lastTick < 1000/60){
+      requestAnimationFrame(animate);
+      return;
+    }
+
+    clearCanvas();
+    for(var i=0; i<loops.length; i++) {
+      loops[i].tick(timestamp);
+    }
+
+
+    lastTick = timestamp;
+    requestAnimationFrame(animate);
+  }
+  
+  this.run = function() {
+    requestAnimationFrame(animate); 
+  }
+  
+  this.reset = function() {
+    loops = [];
+    lastTick = 0
+  
+    for(var i=0; i<50; i++) {
+      loops.push(new InfinityLoop());
+    }
+  }
+  
+  this.reposition = function() {
+    for(i=0; i<loops.length; i++) {
+      loops[i].reposition();
+    }
+  }
+  
+  return this;
+}
+
+
+var scene = new Scene();
+scene.run();
+
+$(window).resize(function(){
+  canvas.width = $(window).width();
+  canvas.height = $(window).height();
+  center[0] = canvas.width/2;
+  center[1] = canvas.height/2;    
+  scene.reposition();
+}).resize();
+
+function reset(){
+  scene.reset();
+}
